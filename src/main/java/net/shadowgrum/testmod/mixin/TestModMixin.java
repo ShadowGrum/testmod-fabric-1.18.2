0958812952ee1d9d6fdd6b0915e250e005406098
ShadowGrum <shadowgrum@gmail.com>
2022-05-03 19:42:24 +0200

    test

---
+++ src/main/java/net/shadowgrum/testmod/mixin/TestModMixin.java
@@ -0,0 +1,16 @@
+package net.shadowgrum.testmod.mixin;
+
+import net.shadowgrum.testmod.TestMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@org.spongepowered.asm.mixin.Mixin(TitleScreen.class)
+public class TestModMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		TestMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
